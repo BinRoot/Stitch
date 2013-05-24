2b8d97e9355549d9a52b2b3090d8fc43b5eea8c2
Nishant Shukla <nick722@gmail.com>
2013-05-23 23:18:17 -0700

    included ejs

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require("express");
+var ejs = require("ejs");
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
     response.send('Hello World!');
 });
+
+app.get('/a', function(request, response) {
+    response.render('index.ejs', {});
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
