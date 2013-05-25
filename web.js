a230e92fdfe5d5dfd04c3640ad4a9fc047d3558b
Nishant Shukla <nick722@gmail.com>
2013-05-25 03:38:18 -0700

    added about page

--- web.js
+++ web.js
@@ -8,6 +8,14 @@ app.get('/', function(request, response) {
     response.render('index.ejs', {});
 });
 
+app.get('/about', function(request, response) {
+    response.render('about.ejs', {});
+});
+
+app.get('/contact', function(request, response) {
+    response.render('contact.ejs', {});
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
