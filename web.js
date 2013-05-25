2656026c3c700e7def7222fef34cca6f4d1600f1
Nishant Shukla <nick722@gmail.com>
2013-05-25 01:38:29 -0700

    Added bootstrap. Trying out fotorama

--- web.js
+++ web.js
@@ -2,15 +2,11 @@ var express = require("express");
 var ejs = require("ejs");
 var app = express();
 app.use(express.logger());
+app.use(express.static(__dirname + '/public')); // This line.
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
-});
-
-app.get('/a', function(request, response) {
     response.render('index.ejs', {});
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
